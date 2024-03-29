2ac5625303a837c2acd6b2668f01596562036fbc
sharath chandran <sharathchandrank91@gmail.com>
2024-03-29 22:52:10 +0530

    validation checking

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,17 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok",
-    });
+    const { username, fullName, email, password } = req.body;
+    console.log("Username :", username);
+
+    if (
+        [username, fullName, email, password].some(
+            (field) => field?.trim() == ""
+        )
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
 });
 
 export { registerUser };
