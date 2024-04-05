81874f629edb769aa4438d5dbc356bba715d678d
sharath chandran <sharathchandrank91@gmail.com>
2024-04-05 19:02:22 +0530

    added watch history endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -434,7 +434,47 @@ const getWatchHistory = asyncHandler(async (req, res) => {
                 _id: mongoose.Types.ObjectId(req.user._id),
             },
         },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        username: 1,
+                                        fullName: 1,
+                                        avatar: 1,
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner",
+                            },
+                        },
+                    },
+                ],
+            },
+        },
     ]);
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200, user[0].watchHistory, "watch history fetched successfully  "
+    ))
 });
 
 export {
