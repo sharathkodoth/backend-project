c1cf95042383c5eab0cfb5751fb23fec2dd9c399
sharath chandran <sharathchandrank91@gmail.com>
2024-04-05 19:24:29 +0530

    added more routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import {
+    changeAccountDetails,
+    changeAvatar,
+    changeCoverImage,
     changePassword,
     getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
     loginUser,
     logoutUser,
     refreshAccessToken,
@@ -32,6 +37,17 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changePassword);
-router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/account-details").patch(verifyJWT, changeAccountDetails);
+
+router
+    .route("/change-avatar")
+    .patch(verifyJWT, upload.single("avatar"), changeAvatar);
+router
+    .route("/change-cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), changeCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
