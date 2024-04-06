2c9faa10db96921f77dcf91d1991c827100bb18a
sharath chandran <sharathchandrank91@gmail.com>
2024-04-06 16:49:03 +0530

    fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -424,7 +424,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
     return res
         .status(200)
-        .json(new ApiResponse(200, "User channel fetched successfully"));
+        .json(new ApiResponse(200,channel[0], "User channel fetched successfully"));
 });
 
 const getWatchHistory = asyncHandler(async (req, res) => {
