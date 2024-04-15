03dc3dc74a3e8b16948cf09ac37af4f9e0da4bb1
sharath chandran <sharathchandrank91@gmail.com>
2024-04-15 20:12:49 +0530

    added delete comment endpoint

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -129,6 +129,29 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
     const { commentId } = req.params;
+
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID");
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found");
+    }
+
+    if (comment.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(
+            403,
+            "You are not author of this comment"
+        );
+    }
+
+    await Comment.findByIdAndDelete(commentId);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, null, "Comment deleted successfully"));
 });
 
-export { getVideoComments, addComment, updateComment };
+export { getVideoComments, addComment, updateComment, deleteComment };
