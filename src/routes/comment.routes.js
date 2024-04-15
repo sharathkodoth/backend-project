35e5a9875487c6c53d91832df8e17f3b8d5dd583
sharath chandran <sharathchandrank91@gmail.com>
2024-04-15 19:43:40 +0530

    updated routes

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
     addComment,
     getVideoComments,
+    updateComment,
 } from "../controllers/comment.controller.js";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
 
@@ -10,5 +11,6 @@ const router = Router();
 router.use(verifyJWT);
 
 router.route("/:videoId").get(getVideoComments).post(addComment);
+router.route("/:commentId").patch(updateComment);
 
 export default router;
