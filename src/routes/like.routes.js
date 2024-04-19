933df7a03795d3df155fdc8111d67083a6879e59
sharath chandran <sharathchandrank91@gmail.com>
2024-04-19 17:53:12 +0530

    new routes added.

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import {
+    toggleCommentLike,
+    toggleCommunityPostLike,
+    toggleVideoLike,
+} from "../controllers/like.controller.js";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/:videoId").post(toggleVideoLike)
+router.route("/toggle/v/:videoId").post(toggleVideoLike);
+router.route("/toggle/c/:commentId").post(toggleCommentLike);
+router.route("/toggle/cp:communityPostId").post(toggleCommunityPostLike);
 
 export default router;
