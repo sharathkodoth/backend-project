4081e073d0a4a53c642583d2043373699d806f50
sharath chandran <sharathchandrank91@gmail.com>
2024-04-21 23:43:42 +0530

    added new routes

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import {
+    getUserChannelSubscribers,
+    toggleSubscription,
+} from "../controllers/subscription.controller.js";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/c/:channelId").post(toggleSubscription);
+router
+    .route("/c/:channelId")
+    .post(toggleSubscription)
+    .get(getUserChannelSubscribers);
 
 export default router;
