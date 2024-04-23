69214676fad42de699e819c8a89d5d58deb1582a
sharath chandran <sharathchandrank91@gmail.com>
2024-04-23 23:16:36 +0530

    bug fixes

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import {
+    createPlaylist,
+    getUserPlaylists,
+} from "../controllers/playlist.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/").post(createPlaylist);
+router.route("/user/:userId").get(getUserPlaylists);
 
 export default router;
