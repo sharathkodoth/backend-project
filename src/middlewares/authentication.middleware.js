97f4267859fa4bc4ee3fce1a60c933e9f01210ec
sharath chandran <sharathchandrank91@gmail.com>
2024-04-23 22:56:56 +0530

    updated

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -9,7 +9,6 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
         const token =
             req.cookies?.accessToken ||
             req.header("Authorization")?.replace("Bearer ", "");
-        
         if (!token) {
             throw new ApiError(
                 401,
